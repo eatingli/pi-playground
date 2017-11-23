5895cc8ad5866ed3fbd7b63931d50f9b9da133ed
Eating Li <suwako.www@gmail.com>
2017-11-23 16:45:05 +0800

    加入 GPIO 變數，更新readme

--- power-off-trigger/README.md
+++ power-off-trigger/README.md
@@ -16,5 +16,28 @@
 - [參考資料](http://www.cnblogs.com/captain_jack/archive/2011/01/11/1933366.html)
 
 # 使用
+
+## 直接使用
+```bash
+    python3 power-off-trigger.py --trig 10 --period 5 --gpio-pin 23
+```
+## Systemd
+- 將主程式移動到 /etc 底下，並給予執行權限
+```bash
+    sudo mv power-off-trigger.py /etc/power-off-trigger.py
+    sudo chmod +x /etc/power-off-trigger.py
+```
+
+- 修改啟動指令搞 (如果有需要的話)
+```bash
+    sudo nano power-off-trigger.service
+```
+
+- 將啟動指令搞移動到 /etc/lib/systemd/system 底下
+```bash
+    sudo mv power-off-trigger.service /etc/lib/systemd/system/power-off-trigger.service
+```
 - 在 systemd 中啟用
-- 執行 power-off-trigger.py --trig 10 --period 5
+```bash
+    sudo systemctl enable power-off-trigger.service
+```
--- power-off-trigger/power-off-trigger.py
+++ power-off-trigger/power-off-trigger.py
@@ -9,19 +9,22 @@ op.add_option("-t", "--trig", dest="trig", default=10,
                   help="count times to showdown")
 op.add_option("-p", "--period", dest="period", default=5,
                   help="period of counting (second)")
+op.add_option("-g", "--gpio-pin", dest="gpio_pin", default=23,
+                  help="period of counting (second)")
 
 (options, args) = op.parse_args()
 count = 0
 trig = int(options.trig)
 period = float(options.period)
-print("Period: ", str(period), " Trig: ", str(trig))
+gpio_pin = int(options.gpio_pin)
+print( "Pin: ", str(gpio_pin), "Period: ", str(period), " Trig: ", str(trig))
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
 	time.sleep(period)
-	if(GPIO.input(23) ==1):
+	if(GPIO.input(gpio_pin) ==1):
 		count = 0
 	else:
 		count+=1
